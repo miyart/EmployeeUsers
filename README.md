96c4c25a631bdab32af82199d974fec04c5bdb12
miyart <62016078+miyart@users.noreply.github.com>
2021-08-30 13:34:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EmployeeUsers
+ 
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { UsesComponent } from './uses/uses.component';
+import { UsersComponent } from './users/users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent,
+    UsesComponent,
+    UsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
